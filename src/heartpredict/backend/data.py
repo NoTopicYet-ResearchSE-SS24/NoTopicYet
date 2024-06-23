10fe06ea97f2c309518c5817c9057ef95639ba7a
iron3oxide <hello@iron3oxi.de>
2024-06-24 01:16:23 +0200

    refactor(data): Use build methods instead of functions

--- src/heartpredict/backend/data.py
+++ src/heartpredict/backend/data.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from functools import lru_cache
 from pathlib import Path
+from typing import Self
 
 import joblib
 import numpy as np
@@ -10,52 +11,61 @@ from sklearn.preprocessing import StandardScaler
 
 
 @dataclass
-class NumpyMatrix:
+class NumpyMatrix: 
     x: np.ndarray
     y: np.ndarray
 
 
 class ProjectData:
-    def __init__(self, csv_path: Path) -> None:
+    def __init__(self, csv: Path) -> None:
         self.project_path = Path(__file__).parent.parent.parent.parent
-        self.df = pd.read_csv(self.project_path.joinpath(csv_path))
+        self.df = pd.read_csv(self.project_path.joinpath(csv))
+
+
+    @classmethod    
+    @lru_cache
+    def build(
+        cls, csv: Path = Path("data/heart_failure_clinical_records.csv")
+        ) -> Self:
+        return cls(csv)
 
 
 class MLData:
     def __init__(
-            self,
-            project_data: ProjectData,
-            test_size: float,
-            random_seed: int
-        ) -> None:
+        self, project_data: ProjectData, test_size: float, random_seed: int
+    ) -> None:
         self.project_data = project_data
         self.test_size = test_size
         self.random_seed = random_seed
         self.raw = self._get_raw_matrix(project_data.df)
         self.train, self.test = self._get_prepared_matrices()
-        
+
+    @classmethod
+    @lru_cache
+    def build(
+        cls, project_data: ProjectData, test_size: float = 0.2, random_seed: int = 42
+    ) -> Self:
+        return cls(project_data, test_size, random_seed)
+
     def _get_raw_matrix(self, df: pd.DataFrame) -> NumpyMatrix:
-        x = df.drop(columns=['DEATH_EVENT'])
-        y = df['DEATH_EVENT']
+        x = df.drop(columns=["DEATH_EVENT"])
+        y = df["DEATH_EVENT"]
 
         return NumpyMatrix(x.values, y.values)  # type: ignore
-    
 
     def _get_prepared_matrices(self) -> tuple[NumpyMatrix, NumpyMatrix]:
         unscaled_x_train, unscaled_x_test, y_train, y_test = train_test_split(
-             self.raw.x,
-             self.raw.y,
-             test_size=self.test_size,
-             random_state=self.random_seed
-            )
+            self.raw.x,
+            self.raw.y,
+            test_size=self.test_size,
+            random_state=self.random_seed,
+        )
         x_train, x_test = self._scale_input_features(unscaled_x_test, unscaled_x_train)
         return NumpyMatrix(x_train, y_train), NumpyMatrix(x_test, y_test)
-        
-    
 
     def _scale_input_features(
-            self, x_train: np.ndarray, x_test: np.ndarray
-        ) -> tuple[np.ndarray, np.ndarray]:
+        self, x_train: np.ndarray, x_test: np.ndarray
+    ) -> tuple[np.ndarray, np.ndarray]:
         """
         Scale the input features.
         Args:
@@ -74,22 +84,5 @@ class MLData:
         scaler_file = output_dir / "used_scaler.joblib"
         joblib.dump(scaler, scaler_file, compress=False)
 
-        x_test = scaler.transform(x_test)   # type: ignore
+        x_test = scaler.transform(x_test)  # type: ignore
         return x_train, x_test
-        
-
-@lru_cache(typed=True)
-def get_project_data(
-    csv_path: Path = Path("data/heart_failure_clinical_records.csv")
-    ) -> ProjectData:
-    return ProjectData(csv_path)
-
-
-@lru_cache(typed=True)
-def get_ml_data(
-    project_data: ProjectData, test_size: float = 0.2, random_seed: int = 42
-    ) -> MLData:
-    return MLData(project_data, test_size, random_seed)
-
-
-
