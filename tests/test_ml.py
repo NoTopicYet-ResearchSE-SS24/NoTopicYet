ab75c22c8c6d1e0ad0ada1c65e9b5f7de4ad0162
HokageM <m.trzaska663@gmail.com>
2024-06-26 10:27:20 +0200

    fix ruff

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -6,34 +6,35 @@ from heartpredict.backend.ml import MLBackend, load_model
 
 
 def test_load_pretrained_models_seed_42(ml_data_func: Callable[..., MLData]) -> None:
+    model_dir = "results/trained_models"
     data = ml_data_func()
     best_decision_tree_score = 0.984
     decision_tree = load_model(
-        f"results/trained_models/DecisionTreeClassifier_model_{data.random_seed}.joblib"
+        f"{model_dir}/DecisionTreeClassifier_model_{data.random_seed}.joblib"
     )
     assert decision_tree.score(data.valid.x, data.valid.y) == best_decision_tree_score
 
     best_random_forest_score = 0.99
     random_forest = load_model(
-        f"results/trained_models/RandomForestClassifier_model_{data.random_seed}.joblib"
+        f"{model_dir}/RandomForestClassifier_model_{data.random_seed}.joblib"
     )
     assert random_forest.score(data.valid.x, data.valid.y) == best_random_forest_score
 
     best_knn_score = 0.977
     knn = load_model(
-        f"results/trained_models/KNeighborsClassifier_model_{data.random_seed}.joblib"
+        f"{model_dir}/KNeighborsClassifier_model_{data.random_seed}.joblib"
     )
     assert knn.score(data.valid.x, data.valid.y) == best_knn_score
 
     best_lda_score = 0.839
     lda = load_model(
-        f"results/trained_models/LinearDiscriminantAnalysis_model_{data.random_seed}.joblib"
+        f"{model_dir}/LinearDiscriminantAnalysis_model_{data.random_seed}.joblib"
     )
     assert lda.score(data.valid.x, data.valid.y) == best_lda_score
 
     best_qda_score = 0.829
     qda = load_model(
-        f"results/trained_models/QuadraticDiscriminantAnalysis_model_{data.random_seed}.joblib"
+        f"{model_dir}/QuadraticDiscriminantAnalysis_model_{data.random_seed}.joblib"
     )
     assert qda.score(data.valid.x, data.valid.y) == best_qda_score
 
